96e41958a43826a538f375c226edaee12a8a199e
Ramil Gabdrakhmanov <ramil.gabdrakhmanov@flatstack.com>
2016-12-01 13:24:42 +0300

    release-prelease

---
+++ deploy_prerelease.sh
@@ -0,0 +1,11 @@
+echo "Begin deploy prerelease"
+echo "Begin deploy prerelease"
+echo "$CI_PULL_REQUEST"
+echo "Begin deploy prerelease"
+echo "Begin deploy prerelease"
+echo "$CI_PULL_REQUESTS"
+echo "Begin deploy prerelease"
+echo "Begin deploy prerelease"
+echo "$CIRCLE_TAG"
+echo "Begin deploy feature"
+
