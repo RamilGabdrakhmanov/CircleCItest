efb2d757ead891f8192ec777bf6747a92880bd78
Ramil Gabdrakhmanov <ramil.gabdrakhmanov@flatstack.com>
2017-02-06 11:41:28 +0300

    fix rc

--- deploy_rc.sh
+++ deploy_rc.sh
@@ -9,3 +9,11 @@ echo "Begin deploy prerelease"
 echo "$CIRCLE_TAG"
 echo "Begin deploy feature"
 
+
+echo "Begin deploy Release Candidate"
+
+export RC_NUMBER=$(echo $CIRCLE_TAG| cut -d'c' -f 2)
+echo $RC_NUMBER
+
+./gradlew assembleRcRelease
+
